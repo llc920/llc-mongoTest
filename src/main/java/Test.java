cfc087ba32755f2eea848dc267c5c70d1fa6f2cc
llc920 <llc920@126.com>
2016-04-29 20:57:00 +0800

    首次提交

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.printf("hahahah");
+	   Mongo mongo=new MongoClient("localhost",27017);
+	 
+	}
+
+}
